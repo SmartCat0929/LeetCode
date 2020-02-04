3f19c9f6590da0b2f41430bf051761c6187bbe15
smartcat <1027699719@qq.com>
2020-02-04 22:51:33 +0800

    大神做法

---
+++ solution/28. Implement strStr().py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/2/4 11:03
+# @Author  : SmartCat0929
+# @Email   : 1027699719@qq.com
+# @Link    : https://github.com/SmartCat0929
+# @Site    : 
+# @File    : 28. Implement strStr().py
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+            a=haystack.find(needle)
+            return a
+print(Solution().strStr("happynewyear",""))
